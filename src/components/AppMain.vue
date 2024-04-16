5a080329bc58d24f0e1bee6b32b9a19e86c1ca23
MatteoBarducci <mbardu1301@gmail.com>
2024-04-16 07:47:28 +0200

    first commit

---
+++ src/components/AppMain.vue
@@ -0,0 +1,20 @@
+<script>
+    export default{
+        name: 'AppMain',
+        data() {
+            return{
+                
+            }
+        }, 
+    }
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+@use '../style/partials/variables.scss' as *;
+
+    
+</style>
